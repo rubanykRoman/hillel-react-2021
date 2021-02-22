879aaebf85773d7743b8c0cfd07d6f744443b95c
manager-fa <romanrubanyk@gmail.com>
2021-02-22 19:13:28 +0200

    add task-1

---
+++ task-1/my-app/src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+
+             <header>
+                <p>I am header</p>
+            </header>
+
+        )
+    }
+}
