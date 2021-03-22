32871b1e1d75f0094754e6a9e2d3afe109fed3a7
manager-fa <romanrubanyk@gmail.com>
2021-03-22 22:59:22 +0200

    task-5 add hook

--- task-5/my-app/src/modules/board/components/NoteBoard/NoteBoard.js
+++ task-5/my-app/src/modules/board/components/NoteBoard/NoteBoard.js
@@ -1,39 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { getNoteBoard, createItem, deleteItem, updateItem } from '../../services/noteBoardService'
+import React from 'react'
 import NoteBoardContainer from '../NoteBoardContainer'
 import NoteBoardHeader from '../NoteBoardHeader/NoteBoardHeader'
+import useNoteBoard from '../../hooks'
 import './NoteBoard.scss';
 
 
 export default function NoteBoard() {
 
-    const [list, setList] = useState([])
-
-    useEffect(() => {
-        getNoteBoard().then(setList)
-    }, [])
-    
-    function createNote (newItem)  {
-        createItem(newItem).then((data) => {
-            setList([...list, data]);
-        });
-    };
-
-    function deleteNote(id) {
-        deleteItem(id);
-
-        setList(list.filter((item) => item.id !== id));
-    };
-
-    function updateNote(stiker) {
-        const item = list.find((l) => l.id === stiker.id);
-        const newItem = { ...item, description: stiker.description };
-
-        updateItem(newItem).then(() => {
-            setList(list.map((item) => item.id !== stiker.id ? item : newItem))
-        });
-    };
-
+    const {list, createNote, deleteNote, updateNote } = useNoteBoard();
 
     return (
         <>
---
+++ task-5/my-app/src/modules/board/hooks.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import { getNoteBoard, createItem, deleteItem, updateItem } from '../../modules/board/services/noteBoardService'
+
+export default function useNoteBoard() {
+
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        getNoteBoard().then(setList)
+    }, [])
+    
+    function createNote (newItem)  {
+        createItem(newItem).then((data) => {
+            setList([...list, data]);
+        });
+    };
+
+    function deleteNote(id) {
+        deleteItem(id);
+
+        setList(list.filter((item) => item.id !== id));
+    };
+
+    function updateNote(stiker) {
+        const item = list.find((l) => l.id === stiker.id);
+        const newItem = { ...item, description: stiker.description };
+
+        updateItem(newItem).then(() => {
+            setList(list.map((item) => item.id !== stiker.id ? item : newItem))
+        });
+    };
+
+    return {
+        list,
+        createNote,
+        deleteNote,
+        updateNote
+    }
+}
